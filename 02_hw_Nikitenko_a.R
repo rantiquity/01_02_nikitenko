c882ce818219774bc78a09283e63ca395fd3607f
yulia-nikitenko86 <yulianikitenko86@gmail.com>
2026-07-09 17:57:14 +0100

    Add files via upload

--- 02_hw_Nikitenko_a.R
+++ 02_hw_Nikitenko_a.R
@@ -18,7 +18,7 @@ my_data <- works |>
 # Удалите столбец name
 my_data2 <- my_data |>
   # ваш код здесь
-  separate (author, into = c("author", "name"), sep = ",")|>
+  separate (author, into = c("author", "name")) |>
   select (author, title)
 
 # (3) Используйте group_by() и summarise(), чтобы узнать,
